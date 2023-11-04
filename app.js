e1007e635edadbe5f1f972b7cf2b58f93025d53d
AndreasB09 <andreasb09.kodehode@gmail.com>
2023-11-04 22:19:28 +0100

    added images

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
 const cardArray = [
     {
-        name: 'fries'
-        img: 
-    }
-]
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'iamges/hotdog.png',
+    },
+    {
+        name: 'icecream',
+        img: 'images/icecream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'iamges/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'iamges/hotdog.png',
+    },
+    {
+        name: 'icecream',
+        img: 'images/icecream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'iamges/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+ ]
+
+ cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
